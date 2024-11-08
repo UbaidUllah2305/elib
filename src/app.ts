e8cda9394552b2aa794b1c7eb9a2f73b51a38061
Your Username <ubaidullah2305@gmail.com>
2024-11-08 10:15:43 +0500

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
-// Routes
-// Http methods: GET, POST, PUT, DELETE, PATCH
+// Routes     // Http methods: GET, POST, PUT, DELETE, PATCH
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the server",
   });
 });
+
+// Global error handler
+app.use(globalErrorHandler);
 
 export default app;
