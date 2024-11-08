fc3a868561aac64a4428d45b36514af3e38ea40f
Your Username <ubaidullah2305@gmail.com>
2024-11-08 11:00:47 +0500

    add register user validation

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,10 +1,15 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { HttpError } from "http-errors";
 import { config } from "../config/config";
 
 // Global error handler
-const globalErrorHandler = (err: HttpError, req: Request, res: Response) => {
-  const statusCode = err.statusCode || 500;
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction // <-- added next parameter
+) => {
+  const statusCode = err.status || 500;
   res.status(statusCode).json({
     message: err.message,
     errorStack: config.env === "development" ? err.stack : "",
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,18 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
-const createUser = async (req: Request, res: Response) => {
-  res.json({
-    message: "This is the createUser controller",
-  });
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  // Validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  // Process
+
+  // Response
+
+  res.json({ message: "This is the createUser controller" });
 };
 
 export { createUser };
